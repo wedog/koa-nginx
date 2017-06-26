a6a59c399bc2cc75c669091c632e23a5099644e6
yong.liu <ly_wuyi@163.com>
2017-06-26 16:57:14 +0800

    Track 2 files into repository.
    - modified test/index.js
    - modified package.json
    
    Auto commit by GitBook Editor

--- test/index.js
+++ test/index.js
@@ -9,11 +9,11 @@ import proxy from '../index';
 
 test.beforeEach(t => {
     const app = new koa();
+    app.use(bodyParser());
     app.use(async(ctx, next) => {
         if(ctx.url == "/sign"){
             ctx.status = 200;
-            //console.log(ctx.host);
-            await next();
+            ctx.body = ctx.request.body;
         }
     });
     t.context.app = app.listen(3000);
@@ -44,12 +44,36 @@ test.serial('should have option host and context', async t => {
     t.is(res.status, 200);
 });
 
-//todo
-test.serial('bodyParser in front of the proxy Middleware', async t=>{
-    t.pass();
+test.serial('behind bodyparser in proxy Middleware', async t=>{
+    const app = new koa();
+    const options = [{
+        "host": "http://127.0.0.1:3000/",
+        "context": "terminal"
+    }];
+    app.use(proxy.proxy(options));
+    app.use(bodyParser());
+    let server = app.listen();
+    const res = await request(server)
+        .post('/terminal/sign')
+        .send({
+            paramA: "hello",
+            paramB: 2
+        });
+    t.is(res.status, 200);
+    t.is(res.body.paramB, 2);
 });
 
-//todo
-test.serial('behind bodyparser in proxy Middleware', async t=>{
-    t.pass();
+test.serial('bodyParser in front of the proxy Middleware', async t=>{
+    const app = new koa();
+    //app.use(bodyParser());
+    const options = [{
+        "host": "http://127.0.0.1:3000/",
+        "context": "terminal"
+    }];
+    app.use(proxy.proxy(options));
+    let server = app.listen();
+    const res = await request(server)
+        .post('/terminal/sign')
+        .send({});
+    t.fail("error")
 });
